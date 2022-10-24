0ec3680bacb08cdb6572b68c796bcc3b48464d5b
milosradic94 <milos.d.radic@htecgroup.com>
2022-10-24 09:12:16 +0200

    Init commit

---
+++ src/main/java/com/losmilos/flightadvisor/FlightadvisorApplication.java
@@ -0,0 +1,13 @@
+package com.losmilos.flightadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightadvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightadvisorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/losmilos/flightadvisor/FlightadvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losmilos.flightadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightadvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
