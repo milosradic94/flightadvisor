f10195ce7b3dbe901d640642f3362d44923c04a2
milosradic94 <milos.d.radic@htecgroup.com>
2022-12-01 10:01:46 +0100

    Fix status code / Remove unnecessary attribute

--- src/main/java/com/losmilos/flightadvisor/model/domain/City.java
+++ src/main/java/com/losmilos/flightadvisor/model/domain/City.java
@@ -4,8 +4,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Builder
 @Getter @Setter
 public class City {
@@ -17,6 +15,4 @@ public class City {
     private String country;
 
     private String description;
-
-    private List<Comment> comments;
 }
