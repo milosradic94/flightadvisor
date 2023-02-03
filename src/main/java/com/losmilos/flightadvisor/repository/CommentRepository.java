990581968cffabe1c3623b6830cecc350035503b
milosradic94 <milos.d.radic@htecgroup.com>
2023-02-03 14:18:32 +0100

    Remove unused repository method / Add @Repository annotation

--- src/main/java/com/losmilos/flightadvisor/repository/CommentRepository.java
+++ src/main/java/com/losmilos/flightadvisor/repository/CommentRepository.java
@@ -1,8 +1,6 @@
 package com.losmilos.flightadvisor.repository;
 
 import com.losmilos.flightadvisor.model.persistance.CommentEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,8 +12,6 @@ public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
     <T> Optional<T> findByIdAndUserId(Long id, Long userId, Class<T> type);
 
     Optional<CommentEntity> findByIdAndUserId(Long id, Long userId);
-
-    Page<CommentEntity> findByCityIdAndInappropriateFalseOrderByIdDesc(Long cityId, Pageable pageable);
 
     void deleteByIdAndUserId(Long id, Long cityId);
 }
--- src/main/java/com/losmilos/flightadvisor/repository/RouteRepository.java
+++ src/main/java/com/losmilos/flightadvisor/repository/RouteRepository.java
@@ -2,6 +2,8 @@ package com.losmilos.flightadvisor.repository;
 
 import com.losmilos.flightadvisor.model.persistance.RouteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RouteRepository extends JpaRepository<RouteEntity, Long> {
 }
